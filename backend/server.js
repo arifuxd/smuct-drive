4f1e32165905fddfbc0e7d96fdb0c75ade5340b7
arifuxd <arifofficial00@gmail.com>
2025-09-28 03:27:02 +0600

    addes samesite config

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production', 
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 24 * 60 * 60 * 1000 
   } // 24 hours
 }));
